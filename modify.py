27028878b1ad6069f4a736f399a5affe38b50f75
Abbas Mustafa <abbasmsh1@gmail.com>
2024-06-22 20:55:49 +0500

    updated modification

--- modify.py
+++ modify.py
@@ -27,22 +27,24 @@ def home( request: Request):
         return templates.TemplateResponse("modify.html", {"request": request})
 
 @app.get('/check_its/')
-def get_its(request: Request,its: int = Form(...), db: Session = Depends(get_db)):
+def get_its(request: Request,its: int, db: Session = Depends(get_db)):
         person = db.query(Master).filter(Master.ITS == its).first()
         booking = db.query(BookingInfo).filter(BookingInfo.ITS == its).first()
         return templates.TemplateResponse("modify.html", {"request": request, "person":person,"booking":booking})
         
         
 
-@app.post("/delete_booking/")
-def delete_booking(id: int = Form(...), db: Session = Depends(get_db)):
-    booking = db.query(BookingInfo).filter(BookingInfo.id == id).first()
-    if not booking:
-        raise HTTPException(status_code=404, detail="Booking not found")
-    
-    db.delete(booking)
-    db.commit()
-    return RedirectResponse(url="/", status_code=303)
+@app.get('/delete_booking/')
+def delete_booking(booking_id: int, db: Session = Depends(get_db)):
+        print(booking_id)
+        booking = db.query(BookingInfo).filter(BookingInfo.ITS == booking_id).first()
+        if not booking:
+                raise HTTPException(status_code=404, detail="Booking not found")
+        
+        db.delete(booking)
+        db.commit()
+        return RedirectResponse(url="/", status_code=303)
+
 
 if __name__ == "__main__":
     import uvicorn
