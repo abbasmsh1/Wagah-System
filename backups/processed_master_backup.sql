f16491cbe71e70173387141881dd7a6d53c5ff16
Abbas Mustafa <abbasmsh1@gmail.com>
2024-06-22 13:56:47 +0500

    separated bus port

--- backups/processed_master_backup.sql
+++ backups/processed_master_backup.sql
@@ -1,1 +1,1 @@
-INSERT INTO "processed_master" VALUES(1,60404157,'Murtaza','bhai','mamonjee','2025-09-01','SW8NUHE7','2029-02-17','7W88ZZEC5H','','',0,'2024-06-21 20:08:23.000000','murtaza');
+INSERT INTO "processed_master" VALUES(1,50471384,'Alefiya','bai','Tayyebjee','2028-09-21','DHKX35KQ','2032-03-04','E405VCQHYW','','',0,'2024-06-21 20:08:23.000000','murtaza');
