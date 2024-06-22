abbcdeb477e5fb192c82546a5ec02a99bed7aa41
Abbas Mustafa <abbasmsh1@gmail.com>
2024-06-23 02:05:56 +0500

    Updated Counts

--- train.py
+++ train.py
@@ -251,6 +251,14 @@ async def view_train_booking(request: Request, db: Session = Depends(get_db)):
     
     return templates.TemplateResponse('train_bookings_.html', {"request": request, "bookings": booking_details})
 
+@app.get("/count-train/")
+def view_train_count(request: Request, db: Session = Depends(get_db)):
+    try:
+        booking_counts = db.query(BookingInfo.train_id,func.count(BookingInfo.train_id).label("passenger_count")).group_by(BookingInfo.train_id).all()
+        # Render the template and pass the data
+        return templates.TemplateResponse("train_counts.html", {"request": request,"booking_counts": booking_counts})
+    except Exception as e:
+        return {"error": str(e)}
 
 if __name__ == "__main__":
     import uvicorn
