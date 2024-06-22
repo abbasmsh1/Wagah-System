ef5277c5f79338f95b038e6f25e1522fbdc2126d
Abbas Mustafa <abbasmsh1@gmail.com>
2024-06-22 22:48:17 +0500

    updated files

--- backups/processed_master_backup.sql
+++ backups/processed_master_backup.sql
@@ -1,1 +1,3 @@
-INSERT INTO "processed_master" VALUES(1,50471385,'Abbas','bhai','Tayyebjee','2026-06-22','X909U6E5','2034-02-13','KE4222UZ4E','','',1,'2024-06-22 19:31:38.026391','abbas');
+INSERT INTO "processed_master" VALUES(1,60404157,'Murtaza','bhai','mamonjee','2025-09-01','SW8NUHE7','2029-02-17','7W88ZZEC5H','','',0,'2024-06-22 16:11:06.000000','murtaza');
+INSERT INTO "processed_master" VALUES(2,30702188,'Zainab','bai','Khambaty','2028-09-06','ZTIZPWXM','2030-06-12','0LMRIHX0L3','','',0,'2024-06-22 16:11:06.000000','murtaza');
+INSERT INTO "processed_master" VALUES(3,50471383,'Mustafa','bhai','Tayyebjee','2035-03-30','6M98030R','2033-05-30','DUQ3QFBZHZ','','',0,'2024-06-22 16:11:06.000000','murtaza');
