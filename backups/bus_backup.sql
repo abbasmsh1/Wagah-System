35c364f2c5a4366723baaba38d9f06e9a335aca6
Abbas Mustafa <abbasmsh1@gmail.com>
2024-06-23 21:00:47 +0500

    database updated

--- backups/bus_backup.sql
+++ backups/bus_backup.sql
@@ -1,2 +1,3 @@
-INSERT INTO "bus" VALUES(1,1,20,'Business');
-INSERT INTO "bus" VALUES(2,2,28,'Executive');
+INSERT INTO "bus" VALUES(1,1,34,'Business');
+INSERT INTO "bus" VALUES(2,2,36,'Business');
+INSERT INTO "bus" VALUES(3,3,29,'Executive');
--- backups/processed_master_backup.sql
+++ backups/processed_master_backup.sql
@@ -1,5 +1,10 @@
 INSERT INTO "processed_master" VALUES(1,50471384,'Alefiya','bai','Tayyebjee','2028-09-21','DHKX35KQ','2032-03-04','E405VCQHYW','','',0,'2024-06-21 20:08:23.000000','murtaza');
 INSERT INTO "processed_master" VALUES(2,40471137,'Abilfazal','bhai','Alibhai','2025-01-23','IJ2TNNNJ','2030-04-14','MN2A7YWBGA','','',1,'2024-06-22 15:55:37.239513','abbas');
-INSERT INTO "processed_master" VALUES(3,30386145,'Husain','bhai','Khambaty','2025-09-18','7JDY10XN','2034-07-10','2QGEBGK071','','',1,'2024-06-22 03:43:18.187128','abbas');
-INSERT INTO "processed_master" VALUES(4,30386144,'Fatema','bai','Khambaty','2030-09-28','AENXJUOT','2028-07-16','H124PW4FDA','','',0,'2024-06-21 20:08:23.000000','abbas');
-INSERT INTO "processed_master" VALUES(5,30702188,'Zainab','bai','Khambaty','2028-09-06','ZTIZPWXM','2030-06-12','0LMRIHX0L3','','',0,'2024-06-21 20:08:23.000000','abbas');
+INSERT INTO "processed_master" VALUES(3,60427030,'Sakina','bai','Quettawala','2025-05-06','AOYCEVTT','2027-11-05','EENUBCZQ8U','','',0,'2024-06-21 20:08:23.000000','murtaza');
+INSERT INTO "processed_master" VALUES(4,60404249,'Fizza','bai','Jeevanjee','2030-09-25','LR572ZUJ','2031-06-02','W45692UOS9','','',0,'2024-06-21 20:08:23.000000','murtaza');
+INSERT INTO "processed_master" VALUES(5,20354551,'Irfanali','bhai','Shahjahanpurwala','2029-05-04','3HBGE93H','2024-09-30','I1PAUB6CXI','','',0,'2024-06-21 20:08:23.000000','murtaza');
+INSERT INTO "processed_master" VALUES(6,30460901,'Hasan','bhai','Shahjahanpurwala','2029-05-10','X5TPZ2BW','2031-06-16','E4TXJMUVR3','','',0,'2024-06-21 20:08:23.000000','murtaza');
+INSERT INTO "processed_master" VALUES(7,60404168,'Sakina','bai','Mithaiwala','2024-12-22','1TYLYPEC','2025-12-23','L7Z4NL12PZ','','',0,'2024-06-21 20:08:23.000000','abbas');
+INSERT INTO "processed_master" VALUES(8,30386145,'Husain','bhai','Khambaty','2025-09-18','7JDY10XN','2034-07-10','2QGEBGK071','','',1,'2024-06-22 03:43:18.187128','abbas');
+INSERT INTO "processed_master" VALUES(9,40458769,'Huzaifa','bhai','Mandsorewala','2024-09-12','S9AN28QL','2033-05-27','9HLXFMVUZH','','',0,'2024-06-21 20:08:23.000000','abbas');
+INSERT INTO "processed_master" VALUES(10,50471385,'Abbas','bhai','Tayyebjee','2026-06-22','X909U6E5','2034-02-13','KE4222UZ4E','','03117866600',1,'2024-06-22 10:49:47.912194','abbas');
