e12d9e30d1401c291d57ff80b065c759e7b63bcf
burhanuddin-5253 <burhanudddinfidaali@gmail.com>
2024-06-23 15:34:13 +0500

    updated html pages and routes.

--- sim.py
+++ sim.py
@@ -124,7 +124,7 @@ async def update_phone(request: Request, its: int = Form(...), phone_number: str
 @app.get("/phone-list/")
 async def get_phone_list(request: Request, db: Session = Depends(get_db)):
     phone_assigned = db.query(Master).filter(Master.phone.isnot(None), Master.phone != '').order_by(desc(Master.timestamp)).all()
-    return templates.TemplateResponse("sim_list.html", {"request": request, "phone_assigned": phone_assigned})
+    return templates.TemplateResponse("sim_list_.html", {"request": request, "phone_assigned": phone_assigned})
 # async def http_exception_handler(request: Request, exc: HTTPException):
 #     if exc.status_code == 404:
 #         return templates.TemplateResponse("404.html", {"request": request}, status_code=404)
